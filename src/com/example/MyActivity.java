5ded290b4894cc7337373a676a79971cc9cad2d1
griver <griver29@gmail.com>
2012-11-13 15:34:09 +0400

    example of sound volume managment

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,15 +1,44 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    private AudioManager manager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.main);
+        Button btn = (Button)findViewById(R.id.changeVolume);
+        btn.setOnClickListener(new View.OnClickListener() {
+            private boolean  muteOn = false;
+            @Override
+            public void onClick(View view) {
+                muteOn = !muteOn;
+                manager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                //int maxRing = manager.getStreamMaxVolume(AudioManager.STREAM_RING);
+                //int maxVoice = manager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
+                //manager.setStreamMute(AudioManager.STREAM_RING, muteOn);
+                //manager.setStreamMute(AudioManager.STREAM_VOICE_CALL, muteOn);
+                //manager.setStreamVolume(AudioManager.STREAM_RING,0, 0);
+                //manager.setStreamVolume(AudioManager.STREAM_RING,0, 0);
+                manager.adjustVolume(AudioManager.ADJUST_LOWER, 0);
+            }
+        });
+
+
     }
+
+
 }
