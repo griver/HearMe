d898aeb54672d62d7ce4a001d5fa97225ff5d4ff
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2012-11-14 07:39:52 +0400

    Minor updates

--- src/ru/spbau/hm/activities/MyActivity.java
+++ src/ru/spbau/hm/activities/MyActivity.java
@@ -1,11 +1,12 @@
 package ru.spbau.hm.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.app.AlertDialog;
+import ru.spbau.hm.R;
 import ru.spbau.hm.impl.MyAlert;
 import ru.spbau.hm.impl.SimpleTimer;
 
